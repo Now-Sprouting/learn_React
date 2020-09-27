27175cd36ae076dfc35aaa1520d515f093fc5007
Now_Sprouting <15542456716@163.com>
2020-09-27 18:50:25 +0800

    learn-router

--- react-redux(reducer 拆分)/src/store/reducer.js
+++ react-redux(reducer 拆分)/src/store/reducer.js
@@ -1,9 +1,16 @@
+import {combineReducers} from 'redux'
 import {reducer as counterReducer} from './counter'
 import {reducer as homeReducer} from './home'
-function reducer(state= {}, action) {
-    return {
-        counterInfo: counterReducer(state.counterInfo, action),
-        homeInfo: homeReducer(state.homeInfo, action)
-    }
-}
+
+// *通过引入插件的形式合并reducer
+// function reducer(state= {}, action) {
+//     return {
+//         counterInfo: counterReducer(state.counterInfo, action),
+//         homeInfo: homeReducer(state.homeInfo, action)
+//     }
+// }
+const reducer = combineReducers({
+    counterInfo: counterReducer,
+    homeInfo: homeReducer
+})
 export default reducer;
